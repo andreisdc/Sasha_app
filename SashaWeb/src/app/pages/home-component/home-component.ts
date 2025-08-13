a4b24cd4fc6b2ce624ce3fc579921ca88984fd63
Pantazi Octavian-Eduard <octavianpantazi@yahoo.com>
2025-08-13 13:36:27 +0300

    Created home component Added as default route of the application home component

---
+++ SashaWeb/src/app/pages/home-component/home-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-component',
+  imports: [],
+  templateUrl: './home-component.html',
+  styleUrl: './home-component.less'
+})
+
+export class HomeComponent {
+
+}
