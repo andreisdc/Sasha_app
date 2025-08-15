50b7af99987b0617dcbc1f76dba71fb3a6049763
Vlad Opris <163576376+vld2405@users.noreply.github.com>
2025-08-15 19:11:10 +0300

    Add sign-up page and route
    Introduced a new sign-up page component with HTML, LESS styling, and a test spec. Updated routing to include the '/sign-up' path and removed the default app.html placeholder content.
    
    STILL NEED TO WORK ON THE DESIGN

--- SashaWeb/src/app/app.routes.ts
+++ SashaWeb/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SignUpPage } from './sign-up-page/sign-up-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: 'sign-up', 
+        component: SignUpPage 
+    }
+];
