82ee4791c5c2dab5e1de66e064d78a6272f9da2b
Vlad Opris <163576376+vld2405@users.noreply.github.com>
2025-09-12 13:21:55 +0300

    Add SearchSection component and update routing
    Introduces the SearchSection component to the home page, including its template, styles, and test. Updates app.routes.ts to use lazy loading for the sign-up page. Moves sign-up page and form files to the 'pages' directory for better organization.

--- SashaWeb/src/app/app.routes.ts
+++ SashaWeb/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { SignUpPage } from './sign-up-page/sign-up-page';
 
 export const routes: Routes = [
     { path: 'login', loadComponent: () => import('./pages/login/login').then(m => m.Login) },
@@ -10,7 +9,8 @@ export const routes: Routes = [
     },
     { 
         path: 'sign-up', 
-        component: SignUpPage 
+        loadComponent: () =>
+            import('./pages/sign-up-page/sign-up-page').then(m => m.SignUpPage)
     },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home', pathMatch: 'full' },
