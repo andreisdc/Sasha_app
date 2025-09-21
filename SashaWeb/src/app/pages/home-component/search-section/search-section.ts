72b04ec19167dfe23fcaa94fcec355c733285153
Vlad Opris <163576376+vld2405@users.noreply.github.com>
2025-09-21 11:49:52 +0300

    Add guest dropdown menu to search section
    Introduced a new DropdownMenu component for selecting guests (adults, children, infants, pets) in the search section. Updated the search-section layout and styles to integrate the dropdown and improve UI consistency.

--- SashaWeb/src/app/pages/home-component/search-section/search-section.ts
+++ SashaWeb/src/app/pages/home-component/search-section/search-section.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { DropdownMenu } from './dropdown-menu/dropdown-menu';
 
 @Component({
   selector: 'app-search-section',
   imports: [
-    NzIconModule
+    NzIconModule,
+    DropdownMenu
   ],
   templateUrl: './search-section.html',
   styleUrl: './search-section.less'
