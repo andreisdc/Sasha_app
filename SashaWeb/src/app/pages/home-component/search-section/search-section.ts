e4cb353a63b21d86633aa7cb050cae255be31137
Vlad Opris <163576376+vld2405@users.noreply.github.com>
2025-09-29 13:53:58 +0300

    Add Angular Material and improve guest input in search section
    Added @angular/material as a dependency and imported relevant Material modules in the search section component. Refactored the guest input to display a dynamic placeholder based on the total number of guests, and updated the HTML to use a readonly input for guest selection.

--- SashaWeb/src/app/pages/home-component/search-section/search-section.ts
+++ SashaWeb/src/app/pages/home-component/search-section/search-section.ts
@@ -2,10 +2,22 @@ import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { DropdownMenu } from './dropdown-menu/dropdown-menu';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-search-section',
-  imports: [NzIconModule, DropdownMenu, NgOptimizedImage],
+  imports: [
+    NzIconModule,
+    DropdownMenu,
+    NgOptimizedImage,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './search-section.html',
   styleUrl: './search-section.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -23,6 +35,21 @@ export class SearchSection {
   }
 
   showGuestDropdown = signal(false);
+  adultsCount = 0;
+  childrenCount = 0;
+  infantsCount = 0;
+  petsCount = 0;
+  get guestsTotal(): number {
+    return (
+      this.adultsCount + this.childrenCount + this.infantsCount + this.petsCount
+    );
+  }
+
+  get guestsPlaceholder(): string {
+    return this.guestsTotal === 0
+      ? 'How many guests...'
+      : `${this.guestsTotal} guests`;
+  }
 
   toggleGuestDropdown() {
     this.showGuestDropdown.update((value) => !value);
