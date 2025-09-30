19776badda41eaac0d46ca39353ce669957130c4
Vlad Opris <163576376+vld2405@users.noreply.github.com>
2025-09-30 20:41:33 +0300

    Add search dropdown and guest counters to menu
    Introduces a new search dropdown UI and replaces guest input fields with increment/decrement buttons and live counters for adults, children, infants, and pets. Implements counter logic using Angular signals and updates styles for dropdowns and buttons.

--- SashaWeb/src/app/pages/home-component/search-section/dropdown-menu/dropdown-menu.ts
+++ SashaWeb/src/app/pages/home-component/search-section/dropdown-menu/dropdown-menu.ts
@@ -1,4 +1,9 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  signal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-dropdown-menu',
@@ -9,4 +14,35 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 })
 export class DropdownMenu {
   @Input() isGuestsOpen = false;
+  @Input() isSearchActive = true;
+
+  adultsCounter = signal(0);
+  childrenCounter = signal(0);
+  infantsCounter = signal(0);
+  petsCounter = signal(0);
+
+  addAdult() {
+    this.adultsCounter.update((a) => a + 1);
+  }
+  addChild() {
+    this.childrenCounter.update((a) => a + 1);
+  }
+  addInfant() {
+    this.infantsCounter.update((a) => a + 1);
+  }
+  addPet() {
+    this.petsCounter.update((a) => a + 1);
+  }
+  removeAdult() {
+    this.adultsCounter.update((a) => Math.max(0, a - 1));
+  }
+  removeChild() {
+    this.childrenCounter.update((a) => Math.max(0, a - 1));
+  }
+  removeInfant() {
+    this.infantsCounter.update((a) => Math.max(0, a - 1));
+  }
+  removePet() {
+    this.petsCounter.update((a) => Math.max(0, a - 1));
+  }
 }
